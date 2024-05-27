b9fd188bc00866896c2cd76b7d6df149bfe2792b
Fathurrahman Amin <fathur.jtk09@gmail.com>
2024-05-27 10:57:40 +0700

    init

---
+++ learn-basic/target/debug/deps/learn_basic-5366c23ce3feb936.d
@@ -0,0 +1,5 @@
+D:\rust\pzn\rust_playground\learn-basic\target\debug\deps\liblearn_basic-5366c23ce3feb936.rmeta: src\main.rs
+
+D:\rust\pzn\rust_playground\learn-basic\target\debug\deps\learn_basic-5366c23ce3feb936.d: src\main.rs
+
+src\main.rs:
---
+++ learn-basic/target/debug/learn-basic.d
@@ -0,0 +1,1 @@
+D:\rust\pzn\rust_playground\learn-basic\target\debug\learn-basic.exe: D:\rust\pzn\rust_playground\learn-basic\src\main.rs
