3fd43e974f876801ad9d7a44655e058e0f141cc4
Fathurrahman Amin <fathur.jtk09@gmail.com>
2024-06-04 08:56:31 +0700

    remove target

--- learn-basic/target/debug/deps/learn_basic-3dfb1edada6e05d8.d
+++
@@ -1,5 +0,0 @@
-D:\rust\pzn\rust_playground\learn-basic\target\debug\deps\learn_basic-3dfb1edada6e05d8.exe: src\main.rs
-
-D:\rust\pzn\rust_playground\learn-basic\target\debug\deps\learn_basic-3dfb1edada6e05d8.d: src\main.rs
-
-src\main.rs:
--- learn-basic/target/debug/deps/learn_basic-e95b5648b7651d5d.d
+++
@@ -1,5 +0,0 @@
-D:\rust\pzn\rust_playground\learn-basic\target\debug\deps\liblearn_basic-e95b5648b7651d5d.rmeta: src\main.rs
-
-D:\rust\pzn\rust_playground\learn-basic\target\debug\deps\learn_basic-e95b5648b7651d5d.d: src\main.rs
-
-src\main.rs:
